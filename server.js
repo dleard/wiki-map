eee78c6114c7d8e3e914c4d97b9a4f41183ef735
Dylan Leard <dleard@uvic.ca>
2019-01-31 23:29:16 +0000

    define new routes in server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+const mapsRoutes = require("./routes/maps");
+const markersRoutes = require("./routes/markers");
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -37,6 +40,8 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
+app.use("/api/maps", mapsRoutes(knex));
+app.use("/api/markers", markersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
