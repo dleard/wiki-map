cd7a102e1e66df55a9342f38a49943a434b9cb33
Dylan Leard <dleard@uvic.ca>
2019-01-31 02:38:45 +0000

    edit originial knex migration and seed files

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,16 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, handle: 'Nick', password: 'password'}),
+        knex('users').insert({id: 2, handle: 'DemoDan', password: 'password'}),
+        knex('users').insert({id: 3, handle: 'Lindsey', password: 'password'}),
+        knex('users').insert({id: 4, handle: 'Yuhan', password: 'password'}),
+        knex('users').insert({id: 5, handle: 'Cartman', password: 'password'}),
+        knex('users').insert({id: 6, handle: 'Stan', password: 'password'}),
+        knex('users').insert({id: 7, handle: 'Kyle', password: 'password'}),
+        knex('users').insert({id: 8, handle: 'Kenny', password: 'password'}),
+        knex('users').insert({id: 9, handle: 'Dylan', password: 'password'}),
+        knex('users').insert({id: 10, handle: 'Ultron', password: 'visionsucks'})
       ]);
     });
 };
