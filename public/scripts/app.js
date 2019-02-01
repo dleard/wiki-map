1141f96d2a24ad7b801a4b1a01cb7489e726fcc4
nfdoyle <nickdoyle98@gmail.com>
2019-02-01 02:11:04 +0000

    dynamic article listing added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,18 +11,29 @@ $(() => {
   });;
 
 
-  let testMap =
+  let testMap = {
+    creatorid: 1,
+    handle: "DemoDan",
+    imgsrc: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+    id: 1,
+    likes: 5,
+    city: 'Victoria',
+    name: "Places I Hate",
+    startlat: 48.422,
+    startlong: -123.362,
+    type: "Custom"
+   }
 
-  function createMapEntry(mapobject){
+  function createMapEntry(mapObject){
     //do biz
     const article = $('<article>').addClass("maplisting");
     
-    const avatar = $('<img>').addClass("logo").attr('src', /*avatar src*/).appendTo(article);
-    const h3 = $('<h3>').text(/*map name*/).appendTo(article);
+    const avatar = $('<img>').addClass("logo").attr('src', mapObject.imgsrc).attr('width', '50px').attr('height', '50px').appendTo(article);
+    const h3 = $('<h3>').text(mapObject.name).appendTo(article);
     const footer = $('<footer>').appendTo(article);
-    const h4_1 = $('<h4>').text(/*handle*/).appendTo(footer);
-    const h4_2 = $('<h4>').text(/*city*/).appendTo(footer);
-    const h4_3 = $('<h4>').text(/*category*/).appendTo(footer);
+    const h4_1 = $('<h4>').text(mapObject.handle).appendTo(footer);
+    const h4_2 = $('<h4>').text(mapObject.city).appendTo(footer);
+    const h4_3 = $('<h4>').text(mapObject.type).appendTo(footer);
     // const report = $('<i>').addClass(`fas fa-flag`).addClass(`tweet-btn`).addClass(`report-btn`).appendTo(footer);
     // const retweet = $('<i>').addClass(`fas fa-retweet`).addClass(`tweet-btn`).addClass(`retweet-btn`).appendTo(footer);
     // const like = $('<i>').addClass(`fas fa-heart tweet-btn like-btn`).data( "liked", false ).appendTo(footer);
@@ -34,14 +45,16 @@ $(() => {
   // RENDER IMPORT
   //
 
-  function renderTweets(maps){
-    maps.forEach(function(map){
-      var $map = createMapEntry(map);
+  function renderTweets(dataObj){
+    for (const obj of dataObj) {
+      var $map = createMapEntry(obj);
 
       // Test / driver code (temporary)
-      $('.maplist-container').prepend($map);
-    });   
+      $('.maplist-container').prepend($map); 
+    }   
   }
+
+  $('.maplist-container').prepend(createMapEntry(testMap));
 
   //
   // RENDER IMPORT END
